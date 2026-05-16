9a319ffb217712b1248cdfe42ea355e2b04d0a41
Damon Haley <damon.haley@nrel.gov>
2026-05-16 00:47:36 -0600

    Fix: move org-agenda-custom-commands out of custom-set-variables
    Prevents load-theme from resetting agenda commands. The BH-style
    commands (N, h, space-bar) now live in dot-org.el using add-to-list
    inside with-eval-after-load, so they coexist with jira commands
    added by work.el without either clobbering the other.

--- dot-org.el
+++ dot-org.el
@@ -1717,6 +1717,89 @@ Late deadlines first, then scheduled, then non-late deadlines"
 (eval-after-load "org"
   '(require 'ox-gfm nil t))
 
+;;; Org agenda custom commands (Bernt Hansen style)
+;; Moved out of custom-set-variables so load-theme doesn't reset them.
+;; Uses add-to-list so jira commands from work.el aren't overwritten.
+(with-eval-after-load 'org-agenda
+  (dolist (cmd '(("N" "Notes" tags "NOTE"
+                  ((org-agenda-overriding-header "Notes") (org-tags-match-list-sublevels t)))
+                 ("h" "Habits" tags-todo "STYLE=\"habit\""
+                  ((org-agenda-overriding-header "Habits")
+                   (org-agenda-sorting-strategy '(todo-state-down effort-up category-keep))))
+                 (" " "Agenda"
+                  ((agenda "" nil)
+                   (tags "REFILE"
+                         ((org-agenda-overriding-header "Tasks to Refile")
+                          (org-tags-match-list-sublevels nil)))
+                   (tags-todo "-CANCELLED/!"
+                              ((org-agenda-overriding-header "Stuck Projects")
+                               (org-agenda-skip-function 'bh/skip-non-stuck-projects)
+                               (org-agenda-sorting-strategy '(category-keep))))
+                   (tags-todo "-HOLD-CANCELLED/!"
+                              ((org-agenda-overriding-header "Projects")
+                               (org-agenda-skip-function 'bh/skip-non-projects)
+                               (org-tags-match-list-sublevels 'indented)
+                               (org-agenda-sorting-strategy '(category-keep))))
+                   (tags-todo "-CANCELLED/!NEXT"
+                              ((org-agenda-overriding-header
+                                (concat "Project Next Tasks"
+                                        (if bh/hide-scheduled-and-waiting-next-tasks ""
+                                          " (including WAITING and SCHEDULED tasks)")))
+                               (org-agenda-skip-function
+                                'bh/skip-projects-and-habits-and-single-tasks)
+                               (org-tags-match-list-sublevels t)
+                               (org-agenda-todo-ignore-scheduled
+                                bh/hide-scheduled-and-waiting-next-tasks)
+                               (org-agenda-todo-ignore-deadlines
+                                bh/hide-scheduled-and-waiting-next-tasks)
+                               (org-agenda-todo-ignore-with-date
+                                bh/hide-scheduled-and-waiting-next-tasks)
+                               (org-agenda-sorting-strategy
+                                '(todo-state-down effort-up category-keep))))
+                   (tags-todo "-REFILE-CANCELLED-WAITING-HOLD/!"
+                              ((org-agenda-overriding-header
+                                (concat "Project Subtasks"
+                                        (if bh/hide-scheduled-and-waiting-next-tasks ""
+                                          " (including WAITING and SCHEDULED tasks)")))
+                               (org-agenda-skip-function 'bh/skip-non-project-tasks)
+                               (org-agenda-todo-ignore-scheduled
+                                bh/hide-scheduled-and-waiting-next-tasks)
+                               (org-agenda-todo-ignore-deadlines
+                                bh/hide-scheduled-and-waiting-next-tasks)
+                               (org-agenda-todo-ignore-with-date
+                                bh/hide-scheduled-and-waiting-next-tasks)
+                               (org-agenda-sorting-strategy '(category-keep))))
+                   (tags-todo "-REFILE-CANCELLED-WAITING-HOLD/!"
+                              ((org-agenda-overriding-header
+                                (concat "Standalone Tasks"
+                                        (if bh/hide-scheduled-and-waiting-next-tasks ""
+                                          " (including WAITING and SCHEDULED tasks)")))
+                               (org-agenda-skip-function 'bh/skip-project-tasks)
+                               (org-agenda-todo-ignore-scheduled
+                                bh/hide-scheduled-and-waiting-next-tasks)
+                               (org-agenda-todo-ignore-deadlines
+                                bh/hide-scheduled-and-waiting-next-tasks)
+                               (org-agenda-todo-ignore-with-date
+                                bh/hide-scheduled-and-waiting-next-tasks)
+                               (org-agenda-sorting-strategy '(category-keep))))
+                   (tags-todo "-CANCELLED+WAITING|HOLD/!"
+                              ((org-agenda-overriding-header
+                                (concat "Waiting and Postponed Tasks"
+                                        (if bh/hide-scheduled-and-waiting-next-tasks ""
+                                          " (including WAITING and SCHEDULED tasks)")))
+                               (org-agenda-skip-function 'bh/skip-non-tasks)
+                               (org-tags-match-list-sublevels nil)
+                               (org-agenda-todo-ignore-scheduled
+                                bh/hide-scheduled-and-waiting-next-tasks)
+                               (org-agenda-todo-ignore-deadlines
+                                bh/hide-scheduled-and-waiting-next-tasks)))
+                   (tags "-REFILE/"
+                         ((org-agenda-overriding-header "Tasks to Archive")
+                          (org-agenda-skip-function 'bh/skip-non-archivable-tasks)
+                          (org-tags-match-list-sublevels nil))))
+                  nil)))
+    (add-to-list 'org-agenda-custom-commands cmd t)))
+
 (provide 'dot-org)
 
 ;; Local Variables:
